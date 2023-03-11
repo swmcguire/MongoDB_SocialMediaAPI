801a9c81bdb7092b3a7f26a9bdc5e3378c67d425
swmcguire <swmcguire1@comcast.net>
2023-03-10 22:52:19 -0600

    minor tweak

--- models/Thought.js
+++ models/Thought.js
@@ -16,7 +16,7 @@ const thoughtSchema = new Schema(
             type: String, 
             required: true,
         },
-        reactions: [reactionSchema] //---------------- ????  Is this correct?
+        reactions: [reactionSchema] //----------- ????  Is this correct?  Where do we get this from? Virtuals?
 
     },
     {
