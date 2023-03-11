b24a241cddf34577364a1afe1be48b4742eda246
swmcguire <swmcguire1@comcast.net>
2023-03-10 22:50:25 -0600

    updated models - still need to finalize, not sure if they are quite right

---
+++ models/Thought.js
@@ -0,0 +1,30 @@
+const  { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280, 
+            minlength: 1,
+        },
+        createdAt: {
+            type: Date, 
+            default: Date.now,
+        },
+        userName: {
+            type: String, 
+            required: true,
+        },
+        reactions: [reactionSchema] //---------------- ????  Is this correct?
+
+    },
+    {
+    toJSON: {
+        getters: true,
+    },
+    }
+);
+
+const Thought = model('thought', thoughtSchema);
+module.exports = thoughtSchema;
---
+++ models/User.js
@@ -0,0 +1,32 @@
+const  { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+const userSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true, 
+            unique: true, 
+            validate: {
+                validator: () => Promise.resolve(false),
+                message: 'Please enter a valid email'
+            } 
+        },
+        thoughts: [thoughtSchema], //-------------- ????  Is this right?
+        friends: [], //----------------------------- ????? Is this right? 
+    },
+    {toJSON: {
+        getters: true,
+    },
+}
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
