0f0540f06db02cdb8e93f14d24c82ce7a720c620
swmcguire <swmcguire1@comcast.net>
2023-03-27 23:07:02 -0500

    just about there - having some posting issues - will clean up tomorrow

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const  { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
     {
-        userName: {
+        username: {
             type: String,
             unique: true,
             required: true,
@@ -12,11 +12,8 @@ const userSchema = new Schema(
             type: String,
             required: true, 
             unique: true, 
-            validate: {
-                validator: () => Promise.resolve(false),
-                message: 'Please enter a valid email'
-            } 
-        },
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+            },
         thoughts: [
             {
                 type: Schema.Types.ObjectId,
